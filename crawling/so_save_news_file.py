b2074fa0662135905f404aaf6b33ff34e76353d9
jihae <sojihae99@gmail.com>
2022-04-11 12:59:53 +0900

    so_add_save_news_file

--- crawling/so_save_news_file.py
+++ crawling/so_save_news_file.py
@@ -1,4 +1,9 @@
-import datetime
-def_save_news_file():   
+from datetime import datetime
+import pandas as pd
+now = datetime.now()
+file_path =''  #결과 저장할 경로
+
+def save_news_file(file_path,df):
     outputFileName = '%s-%s-%s  %s시 %s분 %s초 merging.xlsx' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
-    df.to_csv(RESULT_PATH+outputFileName) #파일명 저장 (파일명 설정)
+    csv = df.to_csv(file_path+outputFileName)
+    return csv 
