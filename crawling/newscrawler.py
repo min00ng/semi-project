df00efb5a586819a2d7677ab87894338b6e904b5
lee_seunghwan <tmdghks304@gmail.com>
2022-04-11 14:42:52 +0900

    stlye : 코드 폴더 변경

--- crawling/newscrawler.py
+++ crawling/newscrawler.py
@@ -1,27 +1,106 @@
+import sqlalchemy.types
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+
 class newscrawler:
-    def __init__(self):
-        pass
+    def __init__(self,driver,heads):
+        self.heads = heads
+        self.dirver = driver
 
     def select_categry(self):
         pass
 
-    def compare_articles(self):
-        pass
+    def get_article_url(self,url):
+        try:
+            self.dirver.get(url)
+            html = self.dirver.page_source
+            bs = BeautifulSoup(html,"html.parser")
+            div = bs.find("div",{"class" : "section_body"})
+            li = div.findAll("li")
+            url_list = [[] for i in range(len(li))]
+            for i in range(len(li)):
+                dt = li[i].find("dt",{"class" : "photo"})
+                a = dt.find("a")
+                url_list[i] = "https://news.naver.com/" + a["href"]
+        except Exception as e:
+            print(e)
+        finally :
+            return url_list
 
     def go_next_page(self):
         pass
 
-    def get_news(self):
-        pass
+    def get_news(self,url_lst):
+        ############ 데이터를 담을 리스트 생성 ############
+        news_category = []
+        press = []
+        main_title = []
+        news_date = []
+        article_content = []
 
-    def save_url(self):
-        pass
+        try:
+            for url in url_lst:
+                ############### 데이터 프레임 객체 생성 ###############
+                news_data_df = pd.DataFrame()
+                
+                #################### 파싱하기 ####################
 
-    def save_news_db(self):
-        pass
+                # 접근을 위한 사용자 정보 입력하기 
+                res = requests.get(url, headers = self.heads)
+
+                # 파싱 시작
+                bs = BeautifulSoup(res.text,'html.parser')
+
+                ################ 뉴스 카테고리 - news_category ###############
+                category = bs.find('div', {'class': 'article_list'})
+                category = category.text.strip()[:2]
+
+                news_category.append(category)
+
+                ################ 언론사 - press ################
+                press_data = bs.find('div', {'class': 'section'})
+                press_data = press_data.h4.text.split()[0]
+
+                press.append(press_data)
+
+                ################ 기사 제목 - main_title ################
+                title = bs.find('div', {'class': 'article_info'})
+                title = title.h3.text
+
+                main_title.append(title)
+
+                ################ 날짜 - news_date ###############
+                n_date = bs.find('span', {'class', 't11'})
+                n_date = n_date.text.split()[0]
+
+                news_date.append(n_date)
+
+                ################ 본문 - article_content ####################
+                news_article = bs.find('div', {'id': "articleBodyContents"})
+            
+                # 문단 별로 나누어 리스트에 저장하는 방식   - 1
+                # news_article = news_article.text.split('      ')[:]
+            
+                # 그대로 기사 내용 추출하는 방식    - 2
+                news_article = news_article.text
+
+                article_content.append(news_article)
+        except Exception as e:
+            print(e)
+        finally:
+            ##################### 데이터 프레임 만들기 ####################
+            idx = ['뉴스 카테고리', '언론사', '기사제목', '날짜', '본문']  
+            news_data = [news_category, press, main_title, news_date, article_content]
+            
+            for i, n in zip(idx, news_data):
+                news_data_df[i] = n
+
+        ########### get_news 함수의 값으로 기사 정보를 담은 데이터 프레임 반환하기 ##########
+        return news_data_df
 
     def save_news_file(self):
         pass
-    
-    def test_print(self):
-        print("testing")
--- crawling/ns_test.py
+++ crawling/ns_test.py
@@ -1,5 +1,20 @@
 import newscrawler
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
-ns = newscrawler.newscrawler()
+# 드라이버 설정
+driver = webdriver.Chrome(ChromeDriverManager().install())
+heads = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Whale/3.12.129.46 Safari/537.36'}
 
-ns.test_print()
+# url
+url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100"
+
+# 클래스 선언
+ns = newscrawler.newscrawler(driver,heads)
+
+lst = ns.get_article_url(url)
+df = ns.get_news(lst)
+
+print(lst)
+print(df)
+
