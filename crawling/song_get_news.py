d1f7f2c8a91c332ff1088e41b9c67862699af842
chaecircle <scw4799@gmail.com>
2022-04-11 13:27:41 +0900

    fix : get_news 수정

--- crawling/song_get_news.py
+++ crawling/song_get_news.py
@@ -10,15 +10,24 @@
 
 ###################################
 
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 def get_news(url_lst):
+
+    ############ 데이터를 담을 리스트 생성 ############
+    news_category = []
+    press = []
+    main_title = []
+    news_date = []
+    article_content = []
+
     try:
         for url in url_lst:
             ############### 시리즈 객체 생성 ###############
-            news_data_ser = pd.Series()
+            news_data_df = pd.DataFrame()
             
             #################### 파싱하기 ####################
 
@@ -30,46 +39,53 @@ def get_news(url_lst):
             bs = BeautifulSoup(res.text,'html.parser')
 
             ################ 뉴스 카테고리 - news_category ###############
-            news_category = bs.find('div', {'class': 'article_list'})
-            news_category = news_category.text.strip()[:2]
+            category = bs.find('div', {'class': 'article_list'})
+            category = category.text.strip()[:2]
+
+            news_category.append(category)
 
             ################ 언론사 - press ################
-            press = bs.find('div', {'class': 'section'})
-            press = press.h4.text.split()[0]
+            press_data = bs.find('div', {'class': 'section'})
+            press_data = press_data.h4.text.split()[0]
+
+            press.append(press_data)
 
             ################ 기사 제목 - main_title ################
-            main_title = bs.find('div', {'class': 'article_info'})
-            main_title = main_title.h3.text
-            
+            title = bs.find('div', {'class': 'article_info'})
+            title = title.h3.text
+
+            main_title.append(title)
+
             ################ 날짜 - news_date ###############
-            news_date = bs.find('span', {'class', 't11'})
-            news_date = news_date.text.split()[0]
+            n_date = bs.find('span', {'class', 't11'})
+            n_date = n_date.text.split()[0]
+
+            news_date.append(n_date)
 
             ################ 본문 - news_article ####################
             news_article = bs.find('div', {'id': "articleBodyContents"})
            
             # 문단 별로 나누어 리스트에 저장하는 방식   - 1
-            news_article = news_article.text.split('      ')[:]
+            # news_article = news_article.text.split('      ')[:]
            
             # 그대로 기사 내용 추출하는 방식    - 2
             news_article = news_article.text
+
+            article_content.append(news_article)
 
            
     except Exception as e:
         print(e)
 
     finally:
-        idx = ['뉴스 카테고리', '언론사', '기사제목', '날짜', '본문']      
-        news_data = [news_category, press, main_title, news_date, news_article]
-
-        # 시리즈에 추가 
-        for i, n in zip(idx, news_data):
-            news_data_ser[i] = n
+        ##################### 데이터 프레임 만들기 ####################
+        idx = ['뉴스 카테고리', '언론사', '기사제목', '날짜', '본문']  
+        news_data = [news_category, press, main_title, news_date, article_content]
         
-        # 데이터 프레임으로 저장하기 (T = transpose(), 행과 열 위치 바꿔주기, idx가 열이 되게끔)
-        news_data = pd.DataFrame(news_data_ser).T 
+        for i, n in zip(idx, news_data):
+            news_data_df[i] = n
 
     ########### get_news 함수의 값으로 기사 정보를 담은 데이터 프레임 반환하기 ##########
-    return news_data
+    return news_data_df
 
         
