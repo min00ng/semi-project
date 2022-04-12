f09b334c1859db6b49b6461b5f7db4a07cd411e3
wldntls <sddyy1004@naver.com>
2022-04-12 17:24:14 +0900

    fix: shin_padding

--- ans_test/shin_padding.py
+++ ans_test/shin_padding.py
@@ -1,13 +1,18 @@
 import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # df 예시
 df = pd.DataFrame(['복숭아', '바나나', '복숭아' '딸기', '복숭아', '복숭아','복숭아','사과','사과','사과','사과','배','배','배','오리','오리','감자','감자','토마토'], columns = ['과일'])
 
-# 패딩 적용 함수s
+# 패딩 적용 함수
 def padding(df):
   tokenizer = Tokenizer()
   tokenizer.fit_on_texts(df['과일'])
-  encoded = tokenizer.texts_to_sequences(df['과일']) # 뒤에 설정 넣을 수 있음(아래로)
+  encoded = tokenizer.texts_to_sequences(df['과일'])
+  padding = pad_sequences(encoded)
 
-  return encoded
+
+  return padding
+
+padding(df)
