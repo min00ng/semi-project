ec4656e6b3ef65174a486439a9d04b2ccfa38e46
data <minjung@gmail.com>
2022-04-15 10:29:57 +0900

    modify stop words

--- kim_stop_words.py
+++ kim_stop_words.py
@@ -2,9 +2,9 @@ def stemmer(text):
     text = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]","",text)
     okt = Okt()
     maxscore_tokenizer = MaxScoreTokenizer(scores=scores)
-    text = maxscore_tokenizer.tokenize(title[1044])#okt.morphs(text, stem= True)
+    text = maxscore_tokenizer.tokenize(text)#okt.morphs(text, stem= True)
     return text
-
+    
 def remove_stop_words(text):
     word_token = stemmer(text)
     stop_words = pd.read_csv("./korean_stop_words.txt")
