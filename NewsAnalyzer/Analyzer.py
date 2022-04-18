032dc79b91a772f309fdcfb8486552f8d0c944bc
lee_seunghwan <tmdghks304@gmail.com>
2022-04-18 17:49:36 +0900

    feat : 서비스 기획 테스트파일 추가

--- NewsAnalyzer/Analyzer.py
+++ NewsAnalyzer/Analyzer.py
@@ -1,3 +1,17 @@
+from transformers import PreTrainedTokenizerFast
+from transformers import BartForConditionalGeneration
+import torch
+
 class analysis:
     def __init__(self) -> None:
-        pass
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
+        self.model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
+
+    def summarizer(self, text):
+        text = text.replace('\n', ' ')
+        raw_input_ids = self.tokenizer.encode(text)
+        input_ids = [self.tokenizer.bos_token_id] + raw_input_ids + [self.tokenizer.eos_token_id]
+
+        summary_ids = self.model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
+        k = self.tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+        return k
