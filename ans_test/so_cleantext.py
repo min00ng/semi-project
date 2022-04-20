9eb0e01d71312f366013623537bb0b4375af498a
lee_seunghwan <tmdghks304@gmail.com>
2022-04-20 22:26:38 +0900

    style:사용하지 않는 파일 정리

--- ans_test/so_cleantext.py
+++
@@ -1,6 +0,0 @@
-
-import re 
-def clean_text(title):
-    text_rmv = re.sub('[-=+,#/\?:^.@*\"※%~∼ㆍ!【】』㈜©囹圄秋 ■◆◇▷▶◁◀ △▲▽▼<>‘|\(\)\[\]`\'…》→←↑↓↔〓♤♠♡♥♧♣⊙◈▣◐◑☆★\”\“\’·※~ ! @ # $ % ^ & * \ " ]', ' ', title)
-    text_rmv = ' '.join(text_rmv.split())
-    return text_rmv
